62260a2c5707a72961f5d07b9727f543ffe7bdd4
Narvin <narvin_castro@yahoo.com>
2024-10-16 20:05:23 +0800

    Fix SQLAlchemy compatibility issue

--- models/database.py
+++ models/database.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from datetime import datetime
 from flask import session
+from sqlalchemy import Column, Integer, String
 
 db = SQLAlchemy()
 
